943728c8814f2b74a58e5587ef0d5948caba413a
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-01-17 18:02:09 +0100

    création vue index.ejs

---
+++ server.js
@@ -0,0 +1,9 @@
+let app = require('express')();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+    response.render('pages/index', {test: 'Salut'});
+});
+
+app.listen(8080);
