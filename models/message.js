495fa3c28e683fb0bb08bb692f9f26cac71731cb
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-01-17 21:36:55 +0100

    Amélioration de l'affichage des messages :           -   ajout d'avatar           -   affiche il y a combien de temps le messge a été posté

--- models/message.js
+++ models/message.js
@@ -1,6 +1,24 @@
 let connexion = require('../config/db');
+let moment = require('../config/moment');
+
 
 class Message {
+
+    constructor(row) {
+        this.row = row;
+    }
+
+    get id() {
+        return this.row.id;
+    }
+
+    get content() {
+        return this.row.content;
+    }
+
+    get created_at() {
+        return moment(this.row.created_at);
+    }
 
     static create(content, cb) {
         connexion.query(
@@ -16,7 +34,7 @@ class Message {
     static all(cb) {
         connexion.query('SELECT * FROM message', (err, rows) => {
             if (err) throw err;
-            cb(rows);
+            cb(rows.map((row) => new Message(row)));
         });
     }
 
