a20ee027d640612512d906c952da7cc226378632
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-01-17 19:32:55 +0100

    Vérification pour savoir si le message n'est pas vide, sinon redirection + message d'erreur

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
+//variable
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+let session = require('express-session');
 
+//moteur de template
 app.set('view engine', 'ejs');
 
+//middleware
 app.use('/assets', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'aqwzsxedc',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+//routes
 
 app.get('/', (request, response) => {
-    response.render('pages/index', {test: 'Salut'});
+    if (request.session.error) {
+        response.locals.error = request.session.error;
+        request.session.error = undefined;
+    }
+    response.render('pages/index');
+});
+
+app.post('/', (request, response) => {
+    if (request.body.message === undefined || request.body.message === '') {
+        request.session.error = "Vous n'avez pas entré de message ";
+        response.redirect('/');
+    }
 });
 
 app.listen(8080);
