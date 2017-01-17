ecdddb3bc29b83ab22a5444bb87fc5595d533ecc
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-01-17 18:42:30 +0100

    ajout header.ejs footer.ejs

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-let app = require('express')();
+let express = require('express');
+let app = express();
 
 app.set('view engine', 'ejs');
+
+app.use('/assets', express.static('public'));
 
 app.get('/', (request, response) => {
     response.render('pages/index', {test: 'Salut'});
